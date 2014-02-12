d00e1c15fbf01a8b6065cb4cdc348e01c96e7487
Igor Rzegocki <ajgon@irgon.com>
2014-02-13 00:29:49 +0100

    Added everything except torrent fetching

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Building
 
 * Install [Add-On SDK](https://addons.mozilla.org/en-US/developers/docs/sdk/latest/dev-guide/tutorials/installation.html)
-* Activate it
+* Activate it (in bash: `source bin/activate`)
 * To test, type `cfx run`
 * To build new xpi type `cfx xpi`
--- data/js/tab.js
+++ data/js/tab.js
@@ -1,3 +1,5 @@
+self.port.emit('validateAuth', false);
+
 var invokeQuery = function(value) {
     var query = document.getElementById('q');
     value = value === undefined ? query.value : value;
@@ -23,23 +25,45 @@ if (window.location.pathname.match(/manager.html/)) {
     document.getElementById('results').addEventListener('click', function(e) {
         e.preventDefault();
         if(e.target.parentNode.className == 'download') {
-            e.target.className = 'loading';
+            e.target.className = 'button loading';
             self.port.emit('queueTorrent', {url: e.target.getAttribute('href'), hash: e.target.parentNode.getAttribute('id').replace('h-', '')});
         }
     });
 }
 
 if (window.location.pathname.match(/list.html/)) {
+    var resizeBars = function() {
+        Array.prototype.slice.call(document.querySelectorAll('.pbar .bar .text')).forEach(function(bar) {bar.style.width = bar.parentNode.parentNode.offsetWidth + 'px'; });
+    };
+
     self.port.emit('queuedTorrents');
     setInterval(function() {
         self.port.emit('queuedTorrents');
     }, 1 * 60 * 1000);
 
     document.getElementById('results').addEventListener('click', function(e) {
-        e.preventDefault();
-        if(e.target.className.match('remove')) {
-            self.port.emit('removeTorrentFromQueue', {hash: e.target.parentNode.getAttribute('id').replace(/^h-/, '')});
+        if(!e.target.parentNode.className.match('disabled')) {
+            if(e.target.parentNode.className.match('remove')) {
+                e.preventDefault();
+                self.port.emit('removeTorrentFromQueue', {hash: e.target.parentNode.parentNode.getAttribute('id').replace(/^h-/, '')});
+            }
         }
+    });
+
+    window.addEventListener('resize', resizeBars);
+}
+
+if (window.location.pathname.match(/auth.html/)) {
+    document.body.parentNode.className = 'page-loading';
+
+    document.getElementById('auth').addEventListener('submit', function(e) {
+        e.preventDefault();
+        document.getElementById('submit-auth').className = 'button loading';
+        document.getElementById('auth').className = '';
+        self.port.emit('validateAuth', {
+            login: document.getElementById('login').value,
+            password: document.getElementById('password').value
+        });
     });
 }
 
@@ -62,13 +86,17 @@ self.port.on('torrentsList', function(list) {
 
     res.innerHTML = '';
 
+    if (list.length === 0) {
+        rows += '<tr class="noitems"><td colspan="5">No results found for this query.<br>Maybe <a href="#" onclick="window.location.href = \'manager.html\'">Try another one</a>?</td></tr>';
+    }
+
     for(var l = 0; l < list.length; l += 1) {
         rows += '<tr>';
         rows += '<td class="title"><a href="http://torrents.eu/' + list[l].hash + '" target="_blank">' + list[l].title + '</a></td>';
         rows += '<td class="size">' + list[l].size.replace(' ', '') + '</td>';
         rows += '<td class="seeds">' + list[l].seeds + '</td>';
         rows += '<td class="peers">' + list[l].peers + '</td>';
-        rows += '<td class="download" id="h-' + list[l].hash + '"><a href="http://torcache.net/torrent/' + list[l].hash + '.torrent">Download!</a></td>';
+        rows += '<td class="download" id="h-' + list[l].hash + '"><a href="http://torcache.net/torrent/' + list[l].hash + '.torrent" class="button">Download!</a></td>';
         rows += '</tr>';
     }
     res.innerHTML += rows;
@@ -85,25 +113,45 @@ self.port.on('queueResult', function(result) {
     }
 });
 
+var normalize = function(bytes) {
+    var normalized = bytes;
+    var prefixes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+    var p = 0;
+    while (normalized > 1024) {
+        p += 1;
+        normalized = Math.round(normalized / 1024 * 10) / 10;
+    }
+
+    return normalized + prefixes[p] + 'B';
+};
+
 self.port.on('queuedTorrentsList', function(list) {
     var results = document.getElementById('results');
     var res = results.querySelector('tbody');
     var rows = '';
+    var bytes_done;
+    var size_bytes;
 
     res.innerHTML = '';
 
+    if (list.length === 0) {
+        rows += '<tr class="noitems"><td colspan="3">No queued torrents at the moment.<br>Maybe, <a href="manager.html">try another query</a>?</td></tr>';
+    }
+
     for(var l = 0; l < list.length; l += 1) {
+        bytes_done = normalize(list[l].bytes_done);
+        size_bytes = normalize(list[l].size_bytes);
+        down_rate = normalize(list[l].down_rate);
+        added_at = new Date(Date.parse(list[l].added_at.replace(' ', 'T'))).toGMTString().replace(/([0-9]) ([0-9])/, '$1<br>$2');
+        rows += '<tr class="pbar"><td colspan="3"><div class="progress">' + bytes_done + ' / ' + size_bytes + ' (' + down_rate + '/s)<div class="bar" style="width: ' + (list[l].bytes_done / list[l].size_bytes * 100) + '%"><div class="text">' + bytes_done + ' / ' + size_bytes + ' (' + down_rate + '/s)</div></div></div></td></tr>';
         rows += '<tr>';
         rows += '<td class="title">' + list[l].name + '</td>';
-        rows += '<td class="size">' + list[l].size_bytes + '</td>';
-        rows += '<td class="seeds">' + list[l].bytes_done + '</td>';
-        rows += '<td class="peers">' + list[l].added_at + '</td>';
-        rows += '<td class="download">' + list[l].down_rate + '</td>';
-        rows += '<td class="actions" id="h-' + list[l].hash + '"><a href="#" class="remove">Remove</a></td>';
+        rows += '<td class="peers">' + added_at + '</td>';
+        rows += '<td class="actions" id="h-' + list[l].hash + '"><a href="#" class="download' + (list[l].bytes_done !== list[l].size_bytes ? ' disabled' : '') + '"><i class="fa fa-download"></i></a> <a href="#" class="remove"><i class="fa fa-times"></i></a></td>';
         rows += '</tr>';
     }
     res.innerHTML += rows;
-
+    resizeBars();
 });
 
 self.port.on('removedTorrent', function(response) {
@@ -111,8 +159,30 @@ self.port.on('removedTorrent', function(response) {
     if(!response.error && response.hash) {
         row = document.getElementById('h-' + response.hash).parentNode;
         row.className = 'hidden';
+        row.previousSibling.className = 'pbar hidden';
         setTimeout(function() {
-            row.parentNode.removeChild(row);
+            self.port.emit('queuedTorrents');
         }, 1000);
     }
+});
+
+self.port.on('authenticated', function(response) {
+    if(window.location.pathname.match(/auth.html/)) {
+        if(response.success) {
+            window.location.href = 'manager.html';
+        } else {
+            if (!response.initialCheck) {
+                document.getElementById('auth').className = 'shake';
+                document.getElementById('submit-auth').className = 'button';
+                document.getElementById('login-error').style.display = 'block';
+                document.getElementById('login-error-message').innerHTML = response.message;
+            }
+        }
+    } else {
+        if(!response.success) {
+            window.location.href = 'auth.html';
+        }
+    }
+
+    document.body.parentNode.className = '';
 });
